a4d8a136b1fdb8fdcf1718850da40834e00abc11
oscar666666 <oscar88feichang6@gmail.com>
2020-12-07 21:55:07 -0600

    auth token request success

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
 <div id="Login">
-    <b-form @Login="onSubmit" @reset="onReset" v-if="show">
+    <b-form  v-if="show">
       <b-form-group
         id="input-group-1"
         label="Email address:"
         label-for="input-1"
         description="We'll never share your email with anyone else."
       >
+      <!-- @Login="onSubmit"type="email" -->
         <b-form-input
           id="input-1"
           v-model="form.email"
-          type="email"
           required
           placeholder="Enter email"
         ></b-form-input>
@@ -26,8 +26,7 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="Login" variant="primary">Login</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="button" v-on:click="login()" variant="primary">Login</b-button>
     </b-form>
     <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
@@ -36,8 +35,10 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: '#Login',
+  name: 'Login',
     data() {
       return {
         form: {
@@ -48,23 +49,29 @@ export default {
       }
     },
     methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.password = ''
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
+      async login() {
+        //evt.preventDefault()
+        try {
+            let res = await axios({
+                url: 'http://127.0.0.1:8000/api-token-auth/',
+                method: 'POST',
+                data: {
+                        username: this.form.email,
+                        password: this.form.password
+                        //grant_type: 'password'
+                    }
+                })
+                alert(`Token received: ${res.data.token}`)
+            }
+            catch (err) {
+                console.log(err);
+                alert(`Error: ${err}`)
+            }
+        }
+    
       }
     }
-  }
+  
 </script>
 <!-- -------------------------------------------- -->
 
